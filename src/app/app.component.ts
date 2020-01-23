45a78c244a2f358d1e8c297d46cbe00e90608fc1
Morgan Worrell <c-morgan.worrell@charter.com>
2020-01-23 12:53:06 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'MSSE663-S8W1-group-project';
+}
