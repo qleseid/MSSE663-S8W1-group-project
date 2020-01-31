614cbf9482d61ea2a0fb2f092042d5f7df9a29a3
Nicholas Juncos <nicholas.juncos@protonmail.com>
2020-01-31 16:42:31 -0600

    Removed dashes from key and db name

--- backend/environment.ts
+++ backend/environment.ts
@@ -1,2 +1,2 @@
-export const databaseName = 'mongodb://localhost/recipe-book';
-export const databaseSecret = 'recipe-secret';
+export const databaseName = 'mongodb://localhost/recipebook';
+export const databaseSecret = 'recipesecret';
