882b6d50eb255e835c395157ad37fd9e06dc9bdb
Lucas <qleseid@yahoo.com>
2020-02-07 03:08:16 -0700

    Added some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-
-const routes: Routes = [];
+//This routes the app from specific address bar inputs
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: LoginComponent},
+  {path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
