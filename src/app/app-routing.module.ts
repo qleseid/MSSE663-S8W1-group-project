3ec0a4b0e66c3e5914721e7f57661dcdfbcb7c3a
Lucas <qleseid@yahoo.com>
2020-02-07 03:51:17 -0700

    Login component updated, needs backend connection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
-//This routes the app from specific address bar inputs
+// This routes the app from specific address bar inputs
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "login", component: LoginComponent},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    //private authenticationService: AuthenticationService,
+    //private alertService: AlertService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    // reset login status
+    //this.authenticationService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || "/";
   }
-
+/*
+  login()
+  {
+    this.loading = true;
+    this.authenticationService
+      .login(this.model.username, this.model.password)
+      .subscribe(data =>
+        {
+          this.router.navigate([this.returnUrl]);
+        },
+        error =>
+        {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      );
+  }
+  */
 }
